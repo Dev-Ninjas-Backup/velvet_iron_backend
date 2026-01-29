84e869eeb01fb42f7ad890f8aa06418dbdd4537b
shamimrana2006 <shamimranaprofessional@gmail.com>
2026-01-30 01:48:17 +0600

    remove all files

--- src/auth/dto/resend-otp.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class ResendOtpDto {
-  @ApiProperty({ example: 'john.doe@example.com' })
-  @IsNotEmpty()
-  @IsEmail()
-  email: string;
-}
--- src/auth/dto/update-auth.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { registerDto } from './register.dto';
-
-export class UpdateAuthDto extends PartialType(registerDto) {}
